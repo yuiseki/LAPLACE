b81e3d690bce1ccdd3f55739e0d57c3578974b5d
yuiseki <yuiseki@gmail.com>
2023-05-27 18:08:54 +0900

    未来をEmbeddingにして、AIに未来を予測させる

--- tests/chains/FutureExtractor.ts
+++ tests/chains/FutureExtractor.ts
@@ -42,56 +42,58 @@ console.log(newsItems.length);
 
 const docs: Document[] = [];
 for await (const newsItem of newsItems) {
-  const text = `${newsItem.pubDate.split(" ")[0]} 配信
+  try {
+    const text = `${newsItem.pubDate.split(" ")[0]} 配信
 ${newsItem.title}
 ${newsItem.description}`;
-  console.log("----- ----- ----- ----- -----");
-  console.log("Input text:");
-  console.log(text);
-  console.log("");
-  const result = await chain.call({ text });
-  console.log("Output text:", result.text);
-  const lines: string[] = result.text.split("\n");
-  const currentDate = lines
-    .filter((line) => line.includes("CurrentDate:"))[0]
-    .split(": ")[1];
-  const futureNearDate = lines
-    .filter((line) => line.includes("FutureNearDate:"))[0]
-    .split(": ")[1];
-  const futureFarDate = lines
-    .filter((line) => line.includes("FutureFarDate:"))[0]
-    .split(": ")[1];
-  const futureIndicateExpression = lines
-    .filter((line) => line.includes("FutureIndicateExpression:"))[0]
-    .split(": ")[1];
-  const whatHappens = lines
-    .filter((line) => line.includes("WhatHappens:"))[0]
-    .split(": ")[1];
-  const pageContent = `${futureIndicateExpression}、${whatHappens}`;
-  const metadata = {
-    url: `https://www3.nhk.or.jp/news/${newsItem.link}`,
-    title: newsItem.title,
-    description: newsItem.description,
-    pubDate: new Date(newsItem.pubDate),
-    currentDate: new Date(currentDate),
-    futureNearDate: new Date(futureNearDate),
-    futureFarDate: new Date(futureFarDate),
-    futureIndicateExpression,
-    whatHappens,
-  };
-  console.log("");
-  console.log("pageContent:", pageContent);
-  console.log("metadata", metadata);
-
-  console.log("----- ----- ----- ----- -----");
-
-  const doc = new Document({
-    pageContent: pageContent,
-    metadata: metadata,
-  });
-  docs.push(doc);
+    console.log("----- ----- ----- ----- -----");
+    console.log("Input text:");
+    console.log(text);
+    console.log("");
+    const result = await chain.call({ text });
+    console.log("Output text:", result.text);
+    const lines: string[] = result.text.split("\n");
+    const currentDate = lines
+      .filter((line) => line.includes("CurrentDate:"))[0]
+      .split(": ")[1];
+    const futureNearDate = lines
+      .filter((line) => line.includes("FutureNearDate:"))[0]
+      .split(": ")[1];
+    const futureFarDate = lines
+      .filter((line) => line.includes("FutureFarDate:"))[0]
+      .split(": ")[1];
+    const futureIndicateExpression = lines
+      .filter((line) => line.includes("FutureIndicateExpression:"))[0]
+      .split(": ")[1];
+    const whatHappens = lines
+      .filter((line) => line.includes("WhatHappens:"))[0]
+      .split(": ")[1];
+    const pageContent = `${futureIndicateExpression}、${whatHappens}`;
+    const metadata = {
+      url: `https://www3.nhk.or.jp/news/${newsItem.link}`,
+      title: newsItem.title,
+      description: newsItem.description,
+      pubDate: new Date(newsItem.pubDate),
+      currentDate: new Date(currentDate),
+      futureNearDate: new Date(futureNearDate),
+      futureFarDate: new Date(futureFarDate),
+      futureIndicateExpression,
+      whatHappens,
+    };
+    console.log("");
+    console.log("pageContent:", pageContent);
+    console.log("metadata", metadata);
+    console.log("----- ----- ----- ----- -----");
+    const doc = new Document({
+      pageContent: pageContent,
+      metadata: metadata,
+    });
+    docs.push(doc);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
-const directory = "public/data/www3.nhk.or.jp/vector_stores";
+const directory = "public/data/www3.nhk.or.jp/vector_stores/";
 await vectorStore.save(directory);
--- tests/laplace.ts
+++ tests/laplace.ts
@@ -1,79 +1,45 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-import { AgentAction, AgentStep } from "langchain/schema";
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
 import { OpenAI } from "langchain/llms/openai";
-import { AgentExecutor, LLMSingleActionAgent } from "langchain/agents";
-
-import { Wikipedia } from "../src/utils/langchain/tools/wikipedia/index.ts";
-/*
-import { loadResolutionChainTool } from "../../../src/utils/langchain/tools/resolutions/index.ts";
-import { loadSituationChainTool } from "../../../src/utils/langchain/tools/situations/index.ts";
-import { loadSummarizationChainTool } from "../../../src/utils/langchain/tools/summarization/index.ts";
-import { loadDateTimeChainTool } from "../../../src/utils/langchain/tools/datetime/index.ts";
-import { ReliefWeb } from "../../../src/utils/langchain/tools/reliefweb/index.ts";
-*/
-
-import { loadLaplaceAgentChain } from "../src/utils/langchain/agents/laplace/index.ts";
-import { LaplaceOutputParser } from "../src/utils/langchain/agents/laplace/parser.ts";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { loadSummarizationChain } from "langchain/chains";
 
 const llm = new OpenAI({ temperature: 0 });
-const tools = [new Wikipedia()];
-const llmChain = loadLaplaceAgentChain({ llm, tools });
+const directory = "public/data/www3.nhk.or.jp/vector_stores/";
+const vectorStore = await HNSWLib.load(directory, new OpenAIEmbeddings());
 
-const input = "How old is the current UN Secretary General?";
-//const input = "Who is the current Secretary General of the United Nations?";
+const questions = [
+  "来週の日本はどうなる？",
+  "来月の日本はどうなる？",
+  "来年の日本はどうなる？",
+  "来週のアメリカはどうなる？",
+  "来月のアメリカはどうなる？",
+  "来年のアメリカはどうなる？",
+];
 
-const outputParser = new LaplaceOutputParser();
-const firstResult = await llmChain.call({
-  input: input,
-  agent_scratchpad: "",
-  stop: ["\nObservation"],
-  intermediate_steps: [],
-});
+for await (const input of questions) {
+  console.log("Q:", input);
+  const now = new Date();
 
-console.log("\n----- ----- ----- ----- ----- -----\n");
-console.log("Q:", input);
-
-let result = firstResult;
-const steps = [];
-
-while (true) {
-  const output = (await outputParser.parse(result.text)) as AgentAction;
-  if ("returnValues" in output) {
-    console.log("----- -----");
-    console.log("Final Answer:", output.returnValues);
-    console.log("----- -----");
-    break;
-  }
-
-  const actions = [output as AgentAction];
-  const action = actions[0];
-  const observation = await tools[0].call({ input: action.toolInput });
-  const step = { action, observation };
-  steps.push(step);
-
-  const agentScratchpad = steps.reduce(
-    (thoughts, { action, observation }) =>
-      thoughts +
-      [action.log, `\nObservation: ${observation}`, "Thought:"]
-        .join("\n")
-        .replace("\n\n", "\n"),
-    ""
+  const results = await vectorStore.similaritySearch(
+    input,
+    10,
+    (document) =>
+      now.getTime() < new Date(document.metadata.futureFarDate).getTime()
   );
-  console.log("----- -----");
-  console.log("agentScratchpad:", agentScratchpad);
-  console.log("----- -----");
+  const sortedSlicedResults = results
+    .sort(
+      (a, b) =>
+        new Date(a.metadata.futureFarDate).getTime() -
+        new Date(b.metadata.futureFarDate).getTime()
+    )
+    .slice(0, 3);
 
-  const actionResult = await llmChain.call({
-    input: input,
-    agent_scratchpad: agentScratchpad,
-    stop: ["\nObservation"],
-    intermediate_steps: [],
+  const chain = loadSummarizationChain(llm, { type: "stuff" });
+  const res = await chain.call({
+    input_documents: sortedSlicedResults,
   });
-  console.log("----- -----");
-  console.log("resultText:", actionResult.text);
-  console.log("----- -----");
-  result = actionResult;
-  console.log("\n----- ----- ----- ----- ----- -----\n");
+  console.log("A:", res.text);
 }
