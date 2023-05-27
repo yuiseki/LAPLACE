33c9f13991134008c1d7956574b6dd099cc98463
yuiseki <yuiseki@gmail.com>
2023-05-27 17:36:04 +0900

    WIP

--- src/utils/langchain/chains/FutureExtractor/index.ts
+++ src/utils/langchain/chains/FutureExtractor/index.ts
@@ -11,28 +11,71 @@ export const loadFutureExtractorChain = ({
 
 Entity Definition and Output Format:
 CurrentDate: Current date mentioned in the text.
-FutureDate: Estimated most future dates from the text.
-WhatHappens: Concise summary of what event is appointments in future, MUST be same language as input text
+FutureNearDate: Most near future date estimated from the text.
+FutureFarDate: Most far future date estimated from the text.
+FutureIndicateExpression: Expressions indicating the future in the text.
+WhatHappens: Concise summary of what event is appointments in future, SHOULD be includes who, what and where, MUST NOT be includes FutureIndicateExpression, MUST be same language as input text.
 ... (You MUST ALWAYS output only one CurrentDate. If it is unclear, output as Unknown)
-... (this FutureDate/WhatHappens can repeat N times)
+... (You MUST ALWAYS output at least one WhatHappens. If it is unclear, output as Unknown)
+... (this FutureNearDate/FutureFarDate/FutureIndicateExpression/WhatHappens can repeat N times)
 
 Examples:
 -----
 Input text:
-News Delivery Time: 2023-05-27
-Cab fares in Ishikawa Prefecture will be raised for the first time in 15 years starting next month on the 26th.
+2023-05-19 配信
+防衛費増額 財源確保の法案 賛成多数で可決 衆院 財務金融委
+防衛費増額の財源確保に向けて「防衛力強化資金」の創設を盛り込んだ法案は、19日の衆議院財務金融委員会で採決が行われ、自民・公明両党などの賛成多数で可決されました。法案は、来週、本会議でも可決されて参議院に送られる見通しです。
 Output:
-CurrentDate: 2023-05-27
-FutureDate: 2023-06-26
-WhatHappens: Cab fares in Ishikawa Prefecture will be raised for the first time in 15 years.
+CurrentDate: 2023-05-19
+FutureNearDate: 2023-05-22
+FutureFarDate: 2023-05-28
+FutureIndicateExpression: 来週
+WhatHappens: 本会議で、 防衛力強化資金の創設を盛り込んだ法案が可決され、参議院に送られる
 
 Input text:
-News Delivery Time: 2023-05-27
-政府は児童手当の第3子以降への多子加算の対象を、現行の「3歳～小学生」から「0歳～高校生」に拡大する最終調整に入った。月3万円の支給を検討している。政府関係者が明らかにした。来週にも開かれる政府の「こども未来戦略会議」で示す、「こども未来戦略方針」の素案に盛り込む見通し。
+2023-05-26 配信
+自民 財政再建派議員ら“将来的な増税議論も”提言案 大筋了承
+政府が来月「骨太の方針」をまとめるのを前に、自民党内のいわゆる財政再建派の議員らは、財政の持続可能性を維持するため必要があれば将来的な増税の議論も排除しない内容の提言案を大筋で了承しました。
+Output:
+CurrentDate: 2023-05-26
+FutureNearDate: 2023-06-01
+FutureFarDate: 2023-06-30
+FutureIndicateExpression: 来月
+WhatHappens: 政府が、「骨太の方針」をまとめる
+
+Input text:
+2023-05-26 配信
+浜田防衛相 米オースティン国防長官と都内で会談へ
+浜田防衛大臣は、アメリカのオースティン国防長官と来月1日に東京都内で会談することを明らかにしました。日米同盟の抑止力、対処力の強化に向けた具体的な取り組みについて協議することにしています。
+Output:
+CurrentDate: 2023-05-26
+FutureNearDate: 2023-06-01
+FutureFarDate: 2023-06-01
+FutureIndicateExpression: 来月1日
+WhatHappens: 浜田防衛大臣が、東京都内で、アメリカのオースティン国防長官と会談する
+
+Input text:
+2023-05-26 配信
+月面着陸失敗 ベンチャー企業「高度の認識ずれ 燃料尽き落下」
+世界初の民間による月面着陸を目指し、地球からおよそ38万キロ離れた月に着陸船を送ったものの、先月、着陸に失敗した日本のベンチャー企業が会見を開き、着陸船が月面にある崖の上を通過したあと高度の認識にずれが生じ、燃料が尽きて5キロほどの高さから落下したと明らかにしました。この企業では、対策を講じたうえで来年、再び着陸に挑む予定です。
+Output:
+CurrentDate: 2023-05-26
+FutureNearDate: 2024-01-01
+FutureFarDate: 2024-12-31
+FutureIndicateExpression: 来年
+WhatHappens: 世界初の民間による月面着陸に失敗した日本のベンチャー企業が、再び月面着陸に挑む
+
+Input text:
+2023-05-27 配信
+米債務上限問題 “来月5日に債務不履行陥るおそれ” 財務長官
+アメリカのイエレン財務長官は、議会が債務上限の引き上げなどに応じなければ、来月5日に債務の不履行に陥るおそれがあるという最新の見通しを示しました。これまでは、早ければ来月1日が期限だとしてきており、日程上、わずかな余裕が生まれた形です。
 Output:
 CurrentDate: 2023-05-27
-FutureDate: 2023-06-03
-WhatHappens: 「こども未来戦略方針」の素案に、児童手当の第3子以降への多子加算の対象拡大を盛り込む
+FutureNearDate: 2023-06-01
+FutureFarDate: 2023-06-05
+FutureIndicateExpression: 来月1日
+FutureIndicateExpression: 来月5日
+WhatHappens: アメリカの議会が、債務上限の引き上げなどに応じなければ、債務の不履行に陥るおそれがある
 -----
 
 Input text:
--- tests/chains/FutureExtractor.ts
+++ tests/chains/FutureExtractor.ts
@@ -3,19 +3,24 @@ dotenv.config();
 
 import { OpenAI } from "langchain/llms/openai";
 import { loadFutureExtractorChain } from "../../src/utils/langchain/chains/FutureExtractor/index.ts";
-import { load } from "cheerio";
+import { Document } from "langchain/document";
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 
 const llm = new OpenAI({ temperature: 0 });
 const chain = loadFutureExtractorChain({ llm });
 
-const nextWeekTokyoNhkNewsUrl =
+const nextWeekNhkNewsUrl =
   "https://noa-api.nhk.jp/r1/db/_search?q=%28%22%E6%9D%A5%E9%80%B1%22%29&index=news&fields=title%2Cdescription&_source=link%2CpubDate%2Ctitle%2Cdescription&sortkey=pubDate&order=desc&from=0&limit=5";
-const nextMonthTokyoNhkNewsUrl =
-  "https://noa-api.nhk.jp/r1/db/_search?q=%28%22%E6%9D%A5%E6%9C%88%22%29&index=news&fields=title%2Cdescription&_source=link%2CpubDate%2Ctitle%2Cdescription&sortkey=pubDate&order=desc&from=0&limit=5";
+const nextMonthNhkNewsUrl =
+  "https://noa-api.nhk.jp/r1/db/_search?q=%28%22%E6%9D%A5%E6%9C%88%22%29&index=news&fields=title%2Cdescription&_source=link%2CpubDate%2Ctitle%2Cdescription&sortkey=pubDate&order=desc&from=0&limit=15";
+const nextYearNhkNewsUrl =
+  "https://noa-api.nhk.jp/r1/db/_search?q=%28%22%E6%9D%A5%E5%B9%B4%22%29&index=news&fields=title%2Cdescription&_source=link%2CpubDate%2Ctitle%2Cdescription&sortkey=pubDate&order=desc&from=0&limit=30";
 
 const futuresTokyoNhkNewsUrl = [
-  nextWeekTokyoNhkNewsUrl,
-  nextMonthTokyoNhkNewsUrl,
+  nextWeekNhkNewsUrl,
+  nextMonthNhkNewsUrl,
+  nextYearNhkNewsUrl,
 ];
 const newsItems: Array<{
   link: string;
@@ -35,17 +40,58 @@ for await (const futureUrl of futuresTokyoNhkNewsUrl) {
 
 console.log(newsItems.length);
 
+const docs: Document[] = [];
 for await (const newsItem of newsItems) {
-  const text = `${newsItem.title}
-${newsItem.pubDate.split(" ")[0]} 配信
+  const text = `${newsItem.pubDate.split(" ")[0]} 配信
+${newsItem.title}
 ${newsItem.description}`;
   console.log("----- ----- ----- ----- -----");
-  console.log("原文:");
+  console.log("Input text:");
   console.log(text);
-  const result = await chain.call({ text });
   console.log("");
-  console.log("結果:");
-  console.log(result.text);
+  const result = await chain.call({ text });
+  console.log("Output text:", result.text);
+  const lines: string[] = result.text.split("\n");
+  const currentDate = lines
+    .filter((line) => line.includes("CurrentDate:"))[0]
+    .split(": ")[1];
+  const futureNearDate = lines
+    .filter((line) => line.includes("FutureNearDate:"))[0]
+    .split(": ")[1];
+  const futureFarDate = lines
+    .filter((line) => line.includes("FutureFarDate:"))[0]
+    .split(": ")[1];
+  const futureIndicateExpression = lines
+    .filter((line) => line.includes("FutureIndicateExpression:"))[0]
+    .split(": ")[1];
+  const whatHappens = lines
+    .filter((line) => line.includes("WhatHappens:"))[0]
+    .split(": ")[1];
+  const pageContent = `${futureIndicateExpression}、${whatHappens}`;
+  const metadata = {
+    url: `https://www3.nhk.or.jp/news/${newsItem.link}`,
+    title: newsItem.title,
+    description: newsItem.description,
+    pubDate: new Date(newsItem.pubDate),
+    currentDate: new Date(currentDate),
+    futureNearDate: new Date(futureNearDate),
+    futureFarDate: new Date(futureFarDate),
+    futureIndicateExpression,
+    whatHappens,
+  };
+  console.log("");
+  console.log("pageContent:", pageContent);
+  console.log("metadata", metadata);
+
   console.log("----- ----- ----- ----- -----");
 
+  const doc = new Document({
+    pageContent: pageContent,
+    metadata: metadata,
+  });
+  docs.push(doc);
 }
+
+const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+const directory = "public/data/www3.nhk.or.jp/vector_stores";
+await vectorStore.save(directory);
